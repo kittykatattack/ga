cc0ce4df6f03bfed13fb208b3a01c08a854e0ce7
Rex <dandylion13@gmail.com>
2014-08-13 09:56:04 -0400

    Added cross-browser patch for the WebAudio API

--- ga.js
+++ ga.js
@@ -29,6 +29,10 @@ Courage, my love, you can do it!
 Table of contents
 -----------------
 
+*Prologue: Fixing the WebAudio API*
+
+`AudioContextMonkeyPatch.js`: Chris Wilson's cross browser patch for the WebAudio API.
+
 *Chapter 1: The game engine*
 
 `GA`:The global GA object.
@@ -83,6 +87,111 @@ Table of contents
 `ga.sound`: Access sound files by their file names.
 
 */
+
+/*
+Prologue: Fixing the WebAudio API
+--------------------------
+
+The WebAudio API is so new that it's API is not consistently implemented properly across
+all modern browsers. Thankfully, Chris Wilson's Audio Context Monkey Patch script 
+normalizes the API for maximum compatibility.
+
+https://github.com/cwilso/AudioContext-MonkeyPatch/blob/gh-pages/AudioContextMonkeyPatch.js
+
+It's included here.
+Thank you, Chris!
+
+*/
+
+(function (global, exports, perf) {
+  'use strict';
+
+  function fixSetTarget(param) {
+    if (!param)	// if NYI, just return
+      return;
+    if (!param.setTargetAtTime)
+      param.setTargetAtTime = param.setTargetValueAtTime; 
+  }
+
+  if (window.hasOwnProperty('webkitAudioContext') && 
+      !window.hasOwnProperty('AudioContext')) {
+    window.AudioContext = webkitAudioContext;
+
+    if (!AudioContext.prototype.hasOwnProperty('createGain'))
+      AudioContext.prototype.createGain = AudioContext.prototype.createGainNode;
+    if (!AudioContext.prototype.hasOwnProperty('createDelay'))
+      AudioContext.prototype.createDelay = AudioContext.prototype.createDelayNode;
+    if (!AudioContext.prototype.hasOwnProperty('createScriptProcessor'))
+      AudioContext.prototype.createScriptProcessor = AudioContext.prototype.createJavaScriptNode;
+
+    AudioContext.prototype.internal_createGain = AudioContext.prototype.createGain;
+    AudioContext.prototype.createGain = function() { 
+      var node = this.internal_createGain();
+      fixSetTarget(node.gain);
+      return node;
+    };
+
+    AudioContext.prototype.internal_createDelay = AudioContext.prototype.createDelay;
+    AudioContext.prototype.createDelay = function(maxDelayTime) { 
+      var node = maxDelayTime ? this.internal_createDelay(maxDelayTime) : this.internal_createDelay();
+      fixSetTarget(node.delayTime);
+      return node;
+    };
+
+    AudioContext.prototype.internal_createBufferSource = AudioContext.prototype.createBufferSource;
+    AudioContext.prototype.createBufferSource = function() { 
+      var node = this.internal_createBufferSource();
+      if (!node.start) {
+        node.start = function ( when, offset, duration ) {
+          if ( offset || duration )
+            this.noteGrainOn( when, offset, duration );
+          else
+            this.noteOn( when );
+        }
+      }
+      if (!node.stop)
+        node.stop = node.noteOff;
+      fixSetTarget(node.playbackRate);
+      return node;
+    };
+
+    AudioContext.prototype.internal_createDynamicsCompressor = AudioContext.prototype.createDynamicsCompressor;
+    AudioContext.prototype.createDynamicsCompressor = function() { 
+      var node = this.internal_createDynamicsCompressor();
+      fixSetTarget(node.threshold);
+      fixSetTarget(node.knee);
+      fixSetTarget(node.ratio);
+      fixSetTarget(node.reduction);
+      fixSetTarget(node.attack);
+      fixSetTarget(node.release);
+      return node;
+    };
+
+    AudioContext.prototype.internal_createBiquadFilter = AudioContext.prototype.createBiquadFilter;
+    AudioContext.prototype.createBiquadFilter = function() { 
+      var node = this.internal_createBiquadFilter();
+      fixSetTarget(node.frequency);
+      fixSetTarget(node.detune);
+      fixSetTarget(node.Q);
+      fixSetTarget(node.gain);
+      return node;
+    };
+
+    if (AudioContext.prototype.hasOwnProperty( 'createOscillator' )) {
+      AudioContext.prototype.internal_createOscillator = AudioContext.prototype.createOscillator;
+      AudioContext.prototype.createOscillator = function() { 
+        var node = this.internal_createOscillator();
+        if (!node.start)
+          node.start = node.noteOn; 
+        if (!node.stop)
+          node.stop = node.noteOff;
+        fixSetTarget(node.frequency);
+        fixSetTarget(node.detune);
+        return node;
+      };
+    }
+  }
+}(window));
 
 
 /*
