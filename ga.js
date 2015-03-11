8d014cd327d1dff29cbc6e454ff85b0b47f9064b
Rex <dandylion13@gmail.com>
2015-03-11 17:16:35 -0400

    Added Alien Armada example game

--- ga.js
+++ ga.js
@@ -558,6 +558,8 @@ GA.create = function(width, height, setup, assetsToLoad, load) {
     }
   });
 
+  
+
   /*
   Chapter 2: Sprites
 
@@ -669,17 +671,18 @@ GA.create = function(width, height, setup, assetsToLoad, load) {
     //The `addChild` method lets you add sprites to this container.
 
     o.addChild = function(sprite) {
+
       //Remove the sprite from its current parent, if it has one, and
       //the parent isn't already this object
-
       if (sprite.parent) {
         sprite.parent.removeChild(sprite);
       }
+
       //Make this object the sprite's parent and
       //add it to this object's `children` array.
-
       sprite.parent = o;
       o.children.push(sprite);
+
       //Calculate the sprite's new width and height
       //o.calculateSize();
     };
@@ -692,6 +695,7 @@ GA.create = function(width, height, setup, assetsToLoad, load) {
       } else {
         throw new Error(sprite + "is not a child of " + o);
       }
+
       //Calculate the sprite's new width and height
       //o.calculateSize();
     };
@@ -1049,12 +1053,28 @@ GA.create = function(width, height, setup, assetsToLoad, load) {
   //remove any sprite, or an argument list of sprites, from its parent.
   ga.remove = function(spritesToRemove) {
     var sprites = Array.prototype.slice.call(arguments);
-    if(sprites.length > 1) {
-      sprites.forEach(function(sprite) {
-        sprite.parent.removeChild(sprite);
-      });
-    } else {
-      sprites[0].parent.removeChild(sprites[0]);
+
+    //Remove sprites that's aren't in an array
+    if (!(sprites[0] instanceof Array)) {
+      if(sprites.length > 1) {
+        sprites.forEach(function(sprite) {
+          sprite.parent.removeChild(sprite);
+        });
+      } else {
+        sprites[0].parent.removeChild(sprites[0]);
+      }
+    } 
+
+    //Remove sprites in an array of sprites
+    else {
+      var spritesArray = sprites[0];
+      if (spritesArray.length > 0) {
+        for (var i = spritesArray.length - 1; i >= 0; i--) {
+          var sprite = spritesArray[i];
+          sprite.parent.removeChild(sprite);
+          spritesArray.splice(spritesArray.indexOf(sprite), 1);
+        }
+      }
     }
   };
 
