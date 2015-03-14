0b8e5ed9130768115a61f483f712e17a15c4432e
Sami ur Rahman Qazi <qsrahman@hotmail.com>
2015-03-14 23:19:40 +0500

    spritesheet having frames of different width and height.
    If a spritesheet contains frames of different width and height it will not be rendered correctly unless the changed is made.

--- ga.js
+++ ga.js
@@ -1657,6 +1657,10 @@ GA.create = function(width, height, setup, assetsToLoad, load) {
         else if (g.assets[o.frames[frameNumber]].frame) {
           o.sourceX = g.assets[o.frames[frameNumber]].frame.x;
           o.sourceY = g.assets[o.frames[frameNumber]].frame.y;
+          o.sourceWidth = Q.assetsCache[o.frames[frameNumber]].frame.w;
+          o.sourceHeight = Q.assetsCache[o.frames[frameNumber]].frame.h;
+          o.width = Q.assetsCache[o.frames[frameNumber]].frame.w;
+          o.height = Q.assetsCache[o.frames[frameNumber]].frame.h;
         }
 
         //If neither of the above are true, then each frame must be
