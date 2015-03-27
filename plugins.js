599903919854201e9a704bc5d5158d6f5c0b13f8
Rex <dandylion13@gmail.com>
2015-03-26 20:27:40 -0400

    Moved the Fullscreen API polyfill to plugins.js

--- plugins.js
+++ plugins.js
@@ -57,6 +57,10 @@ The code in this `plugins.js` file is organized into chapters.
 Use your text editor's search features to find what you're looking for.
 Here's the table of contents to get you started:
 
+### Prologue: Polyfills
+
+- Necessary polyfills for some of the API's used in this file. 
+
 ### Chapter 1: Utilities
 
 `move`: Make a sprite or group move (or an array of them) by updating its velocity.
@@ -142,6 +146,125 @@ Here's the table of contents to get you started:
 `enableFullscreen`: Enables fullscreen mode when the user clicks or touches the canvas.
 
 */
+
+/*
+Prologue
+--------
+
+Some necessary polyfills for some of the newer APIs used in this file
+*/
+
+//### Fixing the Fullscreen API.
+//The Fullscreen API is also in flux and has a quirky browser
+//implementations. Here's a fix for it, thanks to Norman Paschke:
+//https://github.com/neovov/Fullscreen-API-Polyfill/blob/master/fullscreen-api-polyfill.js
+
+(function (doc) {
+	// Use JavaScript script mode
+	"use strict";
+
+	/*global Element */
+
+	var pollute = true,
+		api,
+		vendor,
+		apis = {
+			// http://dvcs.w3.org/hg/fullscreen/raw-file/tip/Overview.html
+			w3: {
+				enabled: "fullscreenEnabled",
+				element: "fullscreenElement",
+				request: "requestFullscreen",
+				exit:    "exitFullscreen",
+				events: {
+					change: "fullscreenchange",
+					error:  "fullscreenerror"
+				}
+			},
+			webkit: {
+				enabled: "webkitIsFullScreen",
+				element: "webkitCurrentFullScreenElement",
+				request: "webkitRequestFullScreen",
+				exit:    "webkitCancelFullScreen",
+				events: {
+					change: "webkitfullscreenchange",
+					error:  "webkitfullscreenerror"
+				}
+			},
+			moz: {
+				enabled: "mozFullScreen",
+				element: "mozFullScreenElement",
+				request: "mozRequestFullScreen",
+				exit:    "mozCancelFullScreen",
+				events: {
+					change: "mozfullscreenchange",
+					error:  "mozfullscreenerror"
+				}
+			},
+			ms: {
+				enabled: "msFullscreenEnabled",
+				element: "msFullscreenElement",
+				request: "msRequestFullscreen",
+				exit:    "msExitFullscreen",
+				events: {
+					change: "MSFullscreenChange",
+					error:  "MSFullscreenError"
+				}
+			}
+		},
+		w3 = apis.w3;
+
+	// Loop through each vendor's specific API
+	for (vendor in apis) {
+		// Check if document has the "enabled" property
+		if (apis[vendor].enabled in doc) {
+			// It seems this browser support the fullscreen API
+			api = apis[vendor];
+			break;
+		}
+	}
+
+	function dispatch( type, target ) {
+		var event = doc.createEvent( "Event" );
+
+		event.initEvent( type, true, false );
+		target.dispatchEvent( event );
+	} // end of dispatch()
+
+	function handleChange( e ) {
+		// Recopy the enabled and element values
+		doc[w3.enabled] = doc[api.enabled];
+		doc[w3.element] = doc[api.element];
+
+		dispatch( w3.events.change, e.target );
+	} // end of handleChange()
+
+	function handleError( e ) {
+		dispatch( w3.events.error, e.target );
+	} // end of handleError()
+
+	// Pollute only if the API doesn't already exists
+	if (pollute && !(w3.enabled in doc) && api) {
+		// Add listeners for fullscreen events
+		doc.addEventListener( api.events.change, handleChange, false );
+		doc.addEventListener( api.events.error,  handleError,  false );
+
+		// Copy the default value
+		doc[w3.enabled] = doc[api.enabled];
+		doc[w3.element] = doc[api.element];
+
+		// Match the reference for exitFullscreen
+		doc[w3.exit] = doc[api.exit];
+
+		// Add the request method to the Element's prototype
+		Element.prototype[w3.request] = function () {
+			return this[api.request].apply( this, arguments );
+		};
+	}
+
+	// Return the API found (or undefined if the Fullscreen API is unavailable)
+	return api;
+
+}(document));
 
 GA = GA || {};
 GA.plugins = function(ga) {
